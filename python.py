af79716063ea224465f0d7fa27d6230875eb111c
ujjwalkat07 <ukcode07@gmail.com>
2026-07-18 18:36:27 +0530

    Refactor code to load environment variables and update response handling; replace email agent example in documentation with model description.

--- python.py
+++ python.py
@@ -1,17 +1,18 @@
-import os
+# import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 
-os.environ["GOOGLE_API_KEY"] = "AIzaSyAwZ8ztwQmMuQuxWVYflBx2u-3VMWu1t9k"
+load_dotenv()
 
 model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
-response = model.invoke("write a python code for building AI agent that automate emails and also tell me the usage of it.")
+response = model.invoke("Tell me about yourself in 100 words.")
 
 content = response.content
-if isinstance(content, list):
-    text = "".join(block["text"] for block in content if isinstance(block, dict) and "text" in block)
-else:
-    text = content
+    # if isinstance(content, list):
+    #     text = "".join(block["text"] for block in content if isinstance(block, dict) and "text" in block)
+    # else:
+    #     text = content
 
 with open("read.md", "w", encoding='utf-8') as f:
-    f.write(text)
-print(text)
+    f.write(content[0].get("text"))
+print(content[0].get("text"))
