60648d456167cedf3ac739faa98581906f70f27d
ujjwalkat07 <ukcode07@gmail.com>
2026-07-19 20:00:49 +0530

    Refactor rag.py to streamline PDF loading and improve prompt formatting for AI model interaction

--- RAG/rag.py
+++ RAG/rag.py
@@ -1,6 +1,4 @@
-# import os
 from pathlib import Path
-
 from langchain_core import documents
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
@@ -10,8 +8,8 @@ from langchain_community.document_loaders import PyPDFLoader
 load_dotenv()
 
 # load document from pdf file and print the content of the first page of the document.
-pdf_path = Path(__file__).resolve().parent / "ujjwal-resume.pdf"
-loader = PyPDFLoader(str(pdf_path))
+# pdf_path = Path(__file__).resolve().parent / "ujjwal-resume.pdf"
+loader = PyPDFLoader("RAG/ujjwal-resume.pdf")
 documents = loader.load()
 # print(documents[0].page_content)
 
@@ -20,21 +18,23 @@ model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 # set the prompt template for the model to use. The template consists of a system message and a human message. The system message is used to set the context for the model, while the human message is used to provide the user's input.
 
 template = ChatPromptTemplate(
-        [
-            ("system", "You are a helpful AI bot. and you are given a document to answer questions about it. The document is as follows: {document}"),
-            ("human", "{user_input}"),
-        ]
-    )
+    [
+        (
+            "system",
+            "You are a helpful AI bot. and you are given a document to answer questions about it. The document is as follows: {document}",
+        ),
+        ("human", "{user_input}"),
+    ]
+)
 # for asking question here we used invoke method of the model object and passed the question as a string argument to it.
 while True:
     val = input("Enter your question: ")
-    prompt_value = template.invoke(
-        {
-            "document": documents[0].page_content,
-            "user_input": val,
-        }
+    test_prompt = template.format_messages(
+        document=documents[0].page_content, user_input=val
     )
-    response = model.invoke(prompt_value)
+
+    # passes to AI model and get the response
+    response = model.invoke(test_prompt)
     # Capture the content of the response
     content = response.content
     print(content[0].get("text"))
