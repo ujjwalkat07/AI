03864f19620cab922ea51bf6421d6c7e4775884b
ujjwalkat07 <ukcode07@gmail.com>
2026-07-19 22:07:55 +0530

    Enhance rag.py to include text splitting and embedding generation; update requirements.txt with additional dependencies

--- RAG/rag.py
+++ RAG/rag.py
@@ -1,17 +1,30 @@
 from pathlib import Path
 from langchain_core import documents
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
+# 1. Load
 # load document from pdf file and print the content of the first page of the document.
 # pdf_path = Path(__file__).resolve().parent / "ujjwal-resume.pdf"
 loader = PyPDFLoader("RAG/ujjwal-resume.pdf")
 documents = loader.load()
 # print(documents[0].page_content)
+
+# 2. Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=20)
+texts = text_splitter.split_documents(documents)
+
+# 3. Generate embeddings
+embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-2-preview")
+vector = embeddings.embed_query("hello, world!")
+vector[:5]
+
+
 
 model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite")
 
